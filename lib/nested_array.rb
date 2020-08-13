3aae1044a144a77763ae52137238ca6f704941b8
Andre Pierre <andre.laurence94@gmail.com>
2020-08-13 05:06:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,11 +1,7 @@
 # Examples inspired by U. S. National Organic Standards
 
 ORGANIC_PRODUCE = [
-  "Strawberries",
-  "Potatoes",
-  "Grapes",
-  "Avocadoes",
-  "Asparagus"
+  "Strawberries", "Potatoes", "Grapes" "Avocadoes",  "Asparagus"
 ]
 
 CONVENTIONAL_PRODUCE = [
@@ -42,7 +38,7 @@ end
 def matrix_lookup(matrix, row, column)
 matrix[
   ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE
-  ][1][0]
+  ][0][1]
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end

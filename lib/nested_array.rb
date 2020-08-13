8d75cba055efa18b3787057f0f0d62f6115205ac
Andre Pierre <andre.laurence94@gmail.com>
2020-08-13 05:11:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -5,11 +5,7 @@ ORGANIC_PRODUCE = [
 ]
 
 CONVENTIONAL_PRODUCE = [
-  "Grapefruit",
-  "Pineapple",
-  "Oranges",
-  "Watermelon",
-  "Eggplant"
+  "Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"
 ]
 
 def assembled_matrix
@@ -38,7 +34,7 @@ end
 def matrix_lookup(matrix, row, column)
 matrix = [
   ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE
-  ][1][0]
+  ][0][1]
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end
